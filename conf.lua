b09d3288cd2ad81d0f76c3a9e1c9dd0f34c5176f
lislis <mail@lislis.de>
2018-04-28 19:57:25 +0200

    fix some stuff

--- conf.lua
+++ conf.lua
@@ -10,7 +10,7 @@ function love.conf(t)
     t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
 
     t.window.title = "A MAZE Jam 2018"        -- The window title (string)
-    t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
+    t.window.icon = "icon.png"                 -- Filepath to an image to use as the window's icon (string)
     t.window.width = 800                -- The window width (number)
     t.window.height = 600               -- The window height (number)
     t.window.borderless = false         -- Remove all border visuals from the window (boolean)
